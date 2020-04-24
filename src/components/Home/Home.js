d49c26b7d42b1322fedc754313197e3177c72ddd
Kevin Medina <kmedinakm@hotmail.com>
2020-04-23 23:35:46 -0400

    fixing the styling and layouts

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Home.css";
 
 const Home = () => {
   return (
     <div className="home">
-      <h1>Welcome to the home page.</h1>
+      <div>
+        <div className="box">
+          <h1>Welcome to Recipe Box.</h1>
+          <p>
+            Sign up for free to search through more than 330,000 recipes from
+            across the web.
+          </p>
+          <div>
+            <Link to="/signup-page">Sign Up Now</Link>
+          </div>
+        </div>
+      </div>
+
+      <div></div>
     </div>
   );
 };
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,37 +1,22 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { AuthContext } from "../../context/index";
 import "./NavBar.css";
 
 function Navbar() {
   return (
-    <AuthContext.Consumer>
-      {(context) => {
-        const { isLoggedIn } = context.state;
-        const { handleLogout } = context;
-        return (
-          <div className="navbar">
-            {isLoggedIn ? (
-              <button onClick={handleLogout}>Logout</button>
-            ) : (
-              <nav>
-                <div>
-                  <NavLink className="link" to="/">
-                    Home
-                  </NavLink>
-                  <NavLink className="link" to="/login-page">
-                    Login
-                  </NavLink>
-                  <NavLink className="link" to="/signup-page">
-                    Sign Up
-                  </NavLink>
-                </div>
-              </nav>
-            )}
-          </div>
-        );
-      }}
-    </AuthContext.Consumer>
+    <div className="navbar">
+      <nav>
+        <NavLink className="link" to="/">
+          Home
+        </NavLink>
+        <NavLink className="link" to="/login-page">
+          Login
+        </NavLink>
+        <NavLink className="link" to="/signup-page">
+          Sign Up
+        </NavLink>
+      </nav>
+    </div>
   );
 }
 
