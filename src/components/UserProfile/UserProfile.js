6d070513ce121cd14f964a870a7c665eef8669fb
Kevin Medina <kmedinakm@hotmail.com>
2020-04-28 20:50:31 -0400

    update the layout of the user's dashboad

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import { AuthContext } from "../../context/index";
 import "./UserProfile.css";
 
@@ -25,58 +26,83 @@ export default class UserProfile extends Component {
 
           return (
             <div className="user-profile">
-              <div className="user-header">
-                <h1>Welcome {username} to MY CookBook.</h1>
-                <p>Build your own personal cookbooks to make the best meals.</p>
-              </div>
-
-              <div className="user-info">
-                <div className="boxes">
+              <header>
+                <h1 className="greeting">Welcome {username}</h1>
+                <Link to="/new-recipebook" className="create-btn">
+                  <i className="fas fa-plus fa-fw"></i> Create New
+                </Link>
+              </header>
+              <main>
+                <section className="user-header">
                   <div>
-                    <h3>Recipes: {recipes.length}</h3>
-                    <i className="fas fa-utensils"></i>
-                  </div>
-                  <div>
-                    <h3>Recipe Books: {recipeBooks.length}</h3>
-                    <i className="fas fa-book"></i>
-                  </div>
-                  <div>
-                    <h3>Favorite Recipes: {favorites.length}</h3>
-                    <i className="fas fa-star"></i>
-                  </div>
-                </div>
-                <div>
-                  <header>
-                    <h1>Favorite Recipes</h1>
-                  </header>
-                  <div className="favorites">
-                    {favorites.length !== 0 ? (
-                      favorites.map((recipe, index) => {
-                        return (
-                          <div key={index}>
-                            <div>
-                              <img
-                                src={`https://spoonacular.com/recipeImages/${recipe.image}`}
-                                alt={recipe.title}
-                              />
-                            </div>
-                            <div>
-                              <h4>
-                                {recipe.title.slice(0, 30)}
-                                {recipe.title.length > 25 ? "..." : null}
-                              </h4>
-                            </div>
-                          </div>
-                        );
-                      })
-                    ) : (
+                    <div className="item">
                       <div>
-                        Click the star icon on a recipe to add a favorite
+                        <i className="fas fa-utensils"></i>
                       </div>
-                    )}
+                      <div>
+                        <h3>Recipes </h3>
+                        <span>{recipes.length}</span>
+                      </div>
+                    </div>
+
+                    <div className="item">
+                      <div>
+                        <i className="fas fa-book"></i>
+                      </div>
+                      <div>
+                        <h3>Recipes Books </h3>
+                        <span>{recipeBooks.length}</span>
+                      </div>
+                    </div>
+
+                    <div className="item">
+                      <div>
+                        <i className="fas fa-star"></i>
+                      </div>
+                      <div>
+                        <h3>Favorite Recipes </h3>
+                        <span>{favorites.length}</span>
+                      </div>
+                    </div>
                   </div>
-                </div>
-              </div>
+                </section>
+
+                {/* Favorites Section */}
+                <section className="favorites-section">
+                  <div>Hello</div>
+                  <div>
+                    <header>
+                      <h1>Favorite Recipes</h1>
+                    </header>
+                    <div className="favorites">
+                      {favorites.length !== 0 ? (
+                        favorites.map((recipe, index) => {
+                          return (
+                            <div key={index}>
+                              <div>
+                                <img
+                                  src={`https://spoonacular.com/recipeImages/${recipe.image}`}
+                                  alt={recipe.title}
+                                />
+                              </div>
+                              <div>
+                                <h4>
+                                  {recipe.title.slice(0, 30)}
+                                  {recipe.title.length > 25 ? "..." : null}
+                                </h4>
+                              </div>
+                            </div>
+                          );
+                        })
+                      ) : (
+                        <div>
+                          Click the star icon on a recipe to add a favorite
+                        </div>
+                      )}
+                    </div>
+                  </div>
+                </section>
+              </main>
             </div>
           );
         }}
